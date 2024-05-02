8ce3d742e8787750e74949a469485302cf8c109a
Ivan M. <95609934+cg4tw@users.noreply.github.com>
2024-05-02 13:56:40 +0200

    safe area context
    th3rdwave/react-native-safe-area-context installed for device compatibility

--- BasicApp/App.js
+++ BasicApp/App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+
+  const authorization ="Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz";
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', 
+               'Authorization': authorization},
+    body: JSON.stringify({"username":"365", "password":"1"})
+  }
+
+
+  getKey();
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>authorization</Text>
+
       <StatusBar style="auto" />
     </View>
+
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +36,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+  //Fetch Method for verification key
+  async function getKey(){
+    
+    fetch("https://api.baubuddy.de/index.php/login",requestOptions)
+      .then(response => response.json().oauth)
+      authorization = response;
+}
+
+//Fetch Method for data
+async function getData(){
+  fetch("https://api.baubuddy.de/index.php/login",requestOptions)
+}
+
+
+
