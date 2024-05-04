4684bf0c19e80e86fe1d43264c501e97ecc8c10c
Ivan M. <95609934+cg4tw@users.noreply.github.com>
2024-05-04 02:10:21 +0200

    getData implemented
    credentials:include
    "https://365.1@api.baubuddy.de/dev/index.php/v1/tasks/select/"

--- BasicApp/App.js
+++ BasicApp/App.js
@@ -2,12 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 //import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-var type='';
-var key='';
+
 
 export default function App() {
+
+  let type='';
+  let key='';
+  
+
+  //need to add if to chech for network avalability and to check for 
+  //Initial start of the app
   getKey();
   getData();
+
+  //
+
   //return <SafeAreaProvider>...</SafeAreaProvider>;
   return (
     <View style={styles.container}>
@@ -38,33 +47,39 @@ const styles = StyleSheet.create({
     })
       .then(response => response.json())
       .then(data=>{
-        type=data.oauth.token_type;
-        key=data.oauth.access_token;
+        global.type=data.oauth.token_type;
+        global.key=data.oauth.access_token;
+
+
       })
       .catch(error => console.error('Error:', error));
 }
 
 
+
+let requestOptions = {
+  method: 'GET',
+  headers: {'Content-Type': 'application/json' ,
+  credentials: 'include',
+     'Authorization':type+" "+key,
+
+  },
+};
+
 //Fetch Method for data
 function getData(){
 
-
-  fetch("https://api.baubuddy.de/dev/index.php/v1/tasks/select",{
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', 
-               'Authorization': type+" "+key},
-    body: JSON.stringify({"username":"365", "password":"1"})
-  })
+  fetch("https://365.1@api.baubuddy.de/dev/index.php/v1/tasks/select/",requestOptions)
   
   .then(response => response.json())
   .then(data=>{
-        console.log(data.debug.source);
-        console.log(data);
 
+    console.log(data);
 
       })
       .catch(error => console.error('Error:', error));
 
+      
 }
 
 
