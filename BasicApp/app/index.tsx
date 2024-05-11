c8b3829e538bbc8cd652d2f58db65c7b78745546
Ivan M. <95609934+cg4tw@users.noreply.github.com>
2024-05-11 21:23:08 +0200

    Features
    Added QR scanning capability
    Pull refresh functionality
    and a worker that re-requests the API every hour

--- BasicApp/app/index.tsx
+++ BasicApp/app/index.tsx
@@ -1,16 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, Text, View, TextInput } from 'react-native';
+import { FlatList, Text, View, TextInput, Button, StyleSheet, RefreshControl, Modal } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useState } from 'react'; 
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Camera } from 'expo-camera';
+import React, { useState, useEffect} from 'react'; 
+
 
   let type:string = '';
   let key:string = '';
   let requestOptions:object = {};
+  let intervalId: NodeJS.Timeout;
+  const styles = StyleSheet.create({
+    container: {
+      flex: 0,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white',
+      zIndex: 1,
+      }
+    }
+  );
 
-
-
-  
   const storeData = async (value:object) => {
     try {
       const jsonValue = JSON.stringify(value);
@@ -37,21 +47,46 @@ import React, { useState } from 'react';
       console.log(data));
 
 
-   
 
 export default function Index() {
   //Initial start of the app
   const [data, setData] = React.useState<any[]>([]);
-
   const [filterText, setFilterText] = React.useState('');
+  const [isScannerVisible, setScannerVisible] = useState(false);
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [refreshing, setRefreshing] = React.useState(false);
+  const handleBarCodeScanned = ({ type, data }:  { type: string, data: string }) => {
+    setScannerVisible(false);
+    setFilterText(data);
+    console.log(data);
+  };
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    getData().then(() => {
+      getSavedData().then(fetchedData => {
+        setData(fetchedData);
+        setRefreshing(false);
+      });
+    });
+  }, []);
+
 
   React.useEffect(() => {
     // Call getData and getSavedData inside useEffect
     getData().then(() => {
       getSavedData().then(fetchedData => setData(fetchedData));
     });
-  }, []);
 
+    intervalId = setInterval(() => {
+      console.log('Fetching data...');
+      getData().then(() => {
+        getSavedData().then(fetchedData => setData(fetchedData));
+      });
+    }, 3600000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   const filteredData = data?.filter(item => 
     Object.values(item).some(value => 
@@ -59,37 +94,91 @@ export default function Index() {
     )
   );
 
-  return (
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Sorry, we need camera permissions to make this work!');
+      }else{
+        setHasPermission(status === 'granted');
+      }
+    })();
+  }, []);
 
+  return (
     <SafeAreaProvider>
-      
       <SafeAreaView style={{ backgroundColor: '#f8f8f8' }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         <TextInput
           value={filterText}
           onChangeText={setFilterText}
           placeholder="Filter"
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1, flex: 1, paddingLeft: 10}}
         />
+        </View>
+        <Button title='Scan Qr Code' onPress={async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            switch (status) {
+              case 'granted':
+                setScannerVisible(true);
+                break;
+              case 'denied':
+                console.log('denied');
+                break;
+              case 'undetermined':
+                console.log('undetermined');
+                break;
+            }
+          
+        }}/>
       </SafeAreaView>
-      <View>
-
+      <SafeAreaView>
 {filteredData && (
-          <FlatList
-            data={filteredData}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({ item }) => (
-              <View style={{ padding: 10, margin: 10, backgroundColor: item.colorCode }}>
-               <Text>{item.task}</Text>
-               <Text>{item.title}</Text>
-               <Text>{item.description}</Text>
-              </View>
-            )}
-          />
+     <FlatList
+        data={filteredData}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={{ padding: 10, margin: 10, backgroundColor: item.colorCode }}>
+          <Text>{item.task}</Text>
+          <Text>{item.title}</Text>
+          <Text>{item.description}</Text>
+          </View>
         )}
+        refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          />
+        }
+      />
+        )}
+    </SafeAreaView>
+       
+    {isScannerVisible && hasPermission && (
+       <Modal
+       animationType="slide"
+       transparent={false}
+       visible={isScannerVisible}
+     >
+      <View style={StyleSheet.absoluteFill}>
+            <View style={{flex: 1, backgroundColor: 'white'}} />
+      <BarCodeScanner
+        onBarCodeScanned={handleBarCodeScanned}
+        style={{width: '100%', height: '60%'}}
+      />
+          <View style={{flex: 1, backgroundColor: 'white'}} />
     </View>
-    </SafeAreaProvider>
+      <View>
+        <Text>Place the QR code in cameras view to scan</Text>
+      </View>
+    <Button title='Exit Scanner' onPress={async () => {
+                setScannerVisible(false);
 
-    
+        }}/>
+    </Modal>
+    )}
+      
+    </SafeAreaProvider>
   );
 }
 
@@ -145,9 +234,9 @@ await fetch("https://365.1@api.baubuddy.de/dev/index.php/v1/tasks/select/",reque
 //need to add if to check for network avalability and to check for server response
 //if server response is 200 then we can continue with the app, else load data from local storage
 
-//create filter
 //create qr code scanner
 
 //Solved
 //add local storage
 //create FlatList
+//create filter
